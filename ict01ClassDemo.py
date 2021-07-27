9fdc0fd8cb998a40171534026c66c78ab0e998a2
Christopher Leonardi <uqcleon4@eait.uq.edu.au>
2021-07-27 11:54:27 +1000

    Update ict01ClassDemo.py
    - forward difference added

--- ict01ClassDemo.py
+++ ict01ClassDemo.py
@@ -6,5 +6,27 @@ Created on Tue Jul 27 11:36:37 2021
 """
 
 import numpy as np
-import matplotplot.pyplot as plt
-import
+import matplotlib.pyplot as plt
+
+def f1(x):
+    return x*x*x
+
+# Create arrays for x and f(x)
+h = 1
+x = np.arange(-5,6,h,dtype=float)
+f = f1(x)
+
+# Calculate the forward difference
+forwardD = np.zeros_like(f)
+forwardD[:-1] = (f[1:] - f[:-1])/h
+
+fig,ax =plt.subplots()
+ax.plot(x, f,
+        x[:-1], forwardD[:-1])
+ax.grid()
+plt.show()
+
+
+ 
+    
+    
